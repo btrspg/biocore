62f1390f1b687027fb9c68aca415e8471c86ff28
logan <yuelong.chen.btr@gmail.com>
2020-03-28 13:47:22 +0800

    [M]: gatk add create pon function

--- biocores/softwares/gatk4.py
+++ biocores/softwares/gatk4.py
@@ -54,6 +54,60 @@ class Gatk4(Task):
             java_options=self._default.java_options
         )
 
+    @utils.modify_cmd
+    @utils.special_tmp
+    def cmd_create_pon(self, bams, intervals, af_file, reference, outdir, tmp):
+        '''
+
+        :param bams:
+        :param intervals:
+        :param af_file:
+        :param reference:
+        :param outdir:
+        :param tmp:
+        :return:
+        '''
+        mutect_call = []
+        normal_vcfs = []
+        for i, bam in enumerate(bams):
+            mutect_call.append('{software} Mutect2 --tmp-dir {tmp} --java-options {java_options} '
+                               '-R {reference} '
+                               '-I {bam} --max-mnp-distance 0 '
+                               '-O {outdir}/normal{i}.vcf.gz'.format(software=self._software,
+                                                                     reference=reference,
+                                                                     bam=bam,
+                                                                     outdir=outdir,
+                                                                     i=i,
+                                                                     tmp=tmp,
+                                                                     java_options=self._default.java_options))
+            normal_vcfs.append('-V {outdir}/normal{i}.vcf.gz'.format(outdir=outdir, i=i))
+
+        return r'''
+{mutect_call}
+
+{software} GenomicsDBImport --tmp-dir {tmp} --java-options {java_options} \
+    -R {reference} \
+    -L {intervals} \
+    --genomicsdb-workspace-path {outdir}/pon.db \
+    {vcfs}
+  
+{software} CreateSomaticPanelOfNormals --tmp-dir {tmp} --java-options {java_options} \
+    -R {reference} \
+    --germline-resource {af_file} \
+    -V gendb://{outdir}/pon.db \
+    -O {outdir}/pon.vcf.gz
+        '''.format(
+            mutect_call='\n'.join(mutect_call),
+            software=self._software,
+            tmp=tmp,
+            java_options=self._default.java_options,
+            reference=reference,
+            intervals=intervals,
+            af_file=af_file,
+            outdir=outdir,
+            vcfs=''.join(normal_vcfs)
+        )
+
     @utils.special_tmp
     @utils.modify_cmd
     def cmd_base_recalibrator(self, bam_file, out_bam, reference, known_sites, qc_prefix, tmp='/tmp'):
@@ -258,3 +312,63 @@ class Gatk4(Task):
 
     def __str__(self):
         return 'Genome Analysis Toolkit'
+
+
+def main():
+    from biocores.softwares.default import gatk4Default
+    bams = ['/agdisk/backup/clinic/AS10736/filtered.bam', '/agdisk/backup/clinic/AS10737/filtered.bam',
+            '/agdisk/backup/clinic/AS10740/filtered.bam', '/agdisk/backup/clinic/AS10741/filtered.bam',
+            '/agdisk/backup/clinic/AS10742/filtered.bam', '/agdisk/backup/clinic/AS10743/filtered.bam',
+            '/agdisk/backup/clinic/AS10744/filtered.bam', '/agdisk/backup/clinic/AS10745/filtered.bam',
+            '/agdisk/backup/clinic/AS10746/filtered.bam', '/agdisk/backup/clinic/AS10747/filtered.bam',
+            '/agdisk/backup/clinic/AS10748/filtered.bam', '/agdisk/backup/clinic/AS10749/filtered.bam',
+            '/agdisk/backup/clinic/AS10787/filtered.bam', '/agdisk/backup/clinic/AS10788/filtered.bam',
+            '/agdisk/backup/clinic/AS10789/filtered.bam', '/agdisk/backup/clinic/AS10790/filtered.bam',
+            '/agdisk/backup/clinic/AS10791/filtered.bam', '/agdisk/backup/clinic/AS10792/filtered.bam',
+            '/agdisk/backup/clinic/AS10793/filtered.bam', '/agdisk/backup/clinic/AS10794/filtered.bam',
+            '/agdisk/backup/clinic/AS10795/filtered.bam', '/agdisk/backup/clinic/AS10796/filtered.bam',
+            '/agdisk/backup/clinic/AS10800/filtered.bam', '/agdisk/backup/clinic/AS10801/filtered.bam',
+            '/agdisk/backup/clinic/AS10804/filtered.bam', '/agdisk/backup/clinic/AS10805/filtered.bam',
+            '/agdisk/backup/clinic/AS10806/filtered.bam', '/agdisk/backup/clinic/AS10808/filtered.bam',
+            '/agdisk/backup/clinic/AS10811/filtered.bam', '/agdisk/backup/clinic/AS10812/filtered.bam',
+            '/agdisk/backup/clinic/AS10815/filtered.bam', '/agdisk/backup/clinic/AS10816/filtered.bam',
+            '/agdisk/backup/clinic/AS10817/filtered.bam', '/agdisk/backup/clinic/AS10820/filtered.bam',
+            '/agdisk/backup/clinic/AS10821/filtered.bam', '/agdisk/backup/clinic/AS10822/filtered.bam',
+            '/agdisk/backup/clinic/AS10823/filtered.bam', '/agdisk/backup/clinic/AS10824/filtered.bam',
+            '/agdisk/backup/clinic/AS10825/filtered.bam', '/agdisk/backup/clinic/AS10826/filtered.bam',
+            '/agdisk/backup/clinic/AS10827/filtered.bam', '/agdisk/backup/clinic/AS10831/filtered.bam',
+            '/agdisk/backup/clinic/AS10832/filtered.bam', '/agdisk/backup/clinic/AS10835/filtered.bam',
+            '/agdisk/backup/clinic/AS10838/filtered.bam', '/agdisk/backup/clinic/AS10839/filtered.bam',
+            '/agdisk/backup/clinic/AS10840/filtered.bam', '/agdisk/backup/clinic/AS10841/filtered.bam',
+            '/agdisk/backup/clinic/AS10842/filtered.bam', '/agdisk/backup/clinic/AS10844/filtered.bam',
+            '/agdisk/backup/clinic/AS10851/filtered.bam', '/agdisk/backup/clinic/AS10852/filtered.bam',
+            '/agdisk/backup/clinic/AS10856/filtered.bam', '/agdisk/backup/clinic/AS10862/filtered.bam',
+            '/agdisk/backup/clinic/AS10863/filtered.bam', '/agdisk/backup/clinic/AS10864/filtered.bam',
+            '/agdisk/backup/clinic/AS10866/filtered.bam', '/agdisk/backup/clinic/AS10871/filtered.bam',
+            '/agdisk/backup/clinic/AS10874/filtered.bam', '/agdisk/backup/clinic/AS10880/filtered.bam',
+            '/agdisk/backup/clinic/AS10881/filtered.bam', '/agdisk/backup/clinic/AS10882/filtered.bam',
+            '/agdisk/backup/clinic/AS10883/filtered.bam', '/agdisk/backup/clinic/AS10884/filtered.bam',
+            '/agdisk/backup/clinic/AS10885/filtered.bam', '/agdisk/backup/clinic/AS10891/filtered.bam',
+            '/agdisk/backup/clinic/AS10894/filtered.bam', '/agdisk/backup/clinic/AS10895/filtered.bam',
+            '/agdisk/backup/clinic/AS10901/filtered.bam', '/agdisk/backup/clinic/AS10902/filtered.bam',
+            '/agdisk/backup/clinic/AS10910/filtered.bam', '/agdisk/backup/clinic/AS10911/filtered.bam',
+            '/agdisk/backup/clinic/AS10912/filtered.bam', '/agdisk/backup/clinic/AS10913/filtered.bam',
+            '/agdisk/backup/clinic/AS10914/filtered.bam', '/agdisk/backup/clinic/AS10915/filtered.bam',
+            '/agdisk/backup/clinic/AS10916/filtered.bam', '/agdisk/backup/clinic/AS10917/filtered.bam',
+            '/agdisk/backup/clinic/AS10918/filtered.bam', '/agdisk/backup/clinic/AS10921/filtered.bam',
+            '/agdisk/backup/clinic/AS10922/filtered.bam', '/agdisk/backup/clinic/AS10924/filtered.bam',
+            '/agdisk/backup/clinic/AS10933/filtered.bam', '/agdisk/backup/clinic/AS10934/filtered.bam',
+            '/agdisk/backup/clinic/AS10935/filtered.bam', '/agdisk/backup/clinic/AS10936/filtered.bam',
+            '/agdisk/backup/clinic/AS10937/filtered.bam', '/agdisk/backup/clinic/AS10938/filtered.bam',
+            '/agdisk/backup/clinic/AS10939/filtered.bam', '/agdisk/backup/clinic/AS10940/filtered.bam',
+            '/agdisk/backup/clinic/AS10941/filtered.bam', '/agdisk/backup/clinic/AS10942/filtered.bam',
+            '/agdisk/backup/clinic/AS10943/filtered.bam', '/agdisk/backup/clinic/AS10944/filtered.bam']
+    gatk=Gatk4('gatk4',gatk4Default)
+    print(gatk.cmd_create_pon(bams,'/aegis/database/human/hg19/enrichments/wes/idt/xgen-exome-research-panel-targets.v0.intervals',
+                        '/aegis/database/human/hg19/annotations/hapmap_3.3_hg19_2nochr_pop_stratified_af.vcf',
+                        '/aegis/database/human/hg19/genome/v0/human_g1k_v37.fasta',
+                        '/aegis/temp/pon/pon_idt',
+                        '/aegis/temp/tmp'))
+
+if __name__ == '__main__':
+    main()
